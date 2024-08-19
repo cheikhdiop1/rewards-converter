918cecc56454eae16f40d1931fc9d98f221a94e2
cheikhdiop1 <cheikh_diop1@outlook.com>
2024-08-19 12:13:34 -0400

    Changes

--- src/main/java/RewardValue.java
+++ src/main/java/RewardValue.java
@@ -1,25 +1,24 @@
 public class RewardValue {
     private double cash;
-    private double miles;
+    private int miles;
 
-    private static final double miles_to_cash_rate = 0.0035;
+    static final double miles_to_cash_rate = 0.0035;
 
     public RewardValue(double cash) {
         this.cash = cash;
-        this.miles = cash / miles_to_cash_rate;
+        this.miles = (int) (cash / miles_to_cash_rate);
     }
 
-    public static RewardValue fromMiles(double miles) {
-        RewardValue rewardValue = new RewardValue(miles * miles_to_cash_rate);
-        rewardValue.miles = miles;
-        return rewardValue;
+    public RewardValue(int miles) {
+        this.miles = miles;
+        this.cash = miles * miles_to_cash_rate;
     }
 
     public double getCashValue() {
         return cash;
     }
 
-    public double getMilesValue() {
+    public int getMilesValue() {
         return miles;
     }
 }
